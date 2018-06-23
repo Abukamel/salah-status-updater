8f6093fb8a0d78056f7e235f76f7103f3722cf79
Ahmed Kamel <k.tricky@gmail.com>
2018-06-23 03:39:58 +0300

    Add options page

---
+++ src/options.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import OptionsPage from './components/OptionsPage';
+
+ReactDOM.render(
+  <OptionsPage />,
+  document.getElementById('options') as HTMLElement
+);
